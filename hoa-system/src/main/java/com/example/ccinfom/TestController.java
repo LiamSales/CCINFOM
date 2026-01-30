42a40b652469a4d35bd09f872cb439d648dd5bc8
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-01-30 00:47:21 -0700

    Refactor TestController to use HoaService

--- hoa-system/src/main/java/com/example/ccinfom/TestController.java
+++ hoa-system/src/main/java/com/example/ccinfom/TestController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-  //“This class contains HTTP endpoints.”
 public class TestController {
 
-  //When an HTTP GET request arrives for /, call this method
+    private final HoaService hoaService;
+
+    public TestController(HoaService hoaService) {
+        this.hoaService = hoaService;
+    }
+
     @GetMapping("/")
     public String home() {
-        return "HOA system is alive";
+        return hoaService.getStatus();
     }
 }
