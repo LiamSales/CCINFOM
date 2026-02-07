bde6504067e760fd9da10dc0ad67e09eaf7e02bd
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-02-07 16:31:47 +0800

    Enhance Homeowner class with additional fields

--- hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
+++ hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
@@ -1,9 +1,17 @@
 package com.example.ccinfom;
 
+import java.util.ArrayList;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+
+//Personal information and contact details
+//Years as homeowner
+//Owned properties within the subdivision
+//Declaration of data accuracy and intent to become an HOA member
+//Other addresses and contact information when not residing in the subdivision
 
 @Entity
 public class Homeowner {
@@ -14,12 +22,27 @@ public class Homeowner {
 
     private String name;
 
+    private int yearsAsHomeowner;
+
+    private boolean intentDeclared;
+
+    @OneToMany(mappedBy = "homeowner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Property> properties = new ArrayList<>();
+
+    @OneToMany(mappedBy = "homeowner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ContactInfo> contacts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "homeowner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
     protected Homeowner() {
         // Required by JPA and Jackson
     }
 
-    public Homeowner(String name) {
+    public Homeowner(String name, int yearsAsHomeowner, boolean intentDeclared) {
         this.name = name;
+        this.yearsAsHomeowner = yearsAsHomeowner;
+        this.intentDeclared = intentDeclared;
     }
 
     public Long getId() {
@@ -32,5 +55,16 @@ public class Homeowner {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+        public int getYears() {
+        return yearsAsHomeowner;
+    }
+        public boolean getIntent() {
+        return intentDeclared;
+
+    }
+        public void setIntent(boolean intentDeclared) {
+        this.intentDeclared = intentDeclared;
     }
 }
