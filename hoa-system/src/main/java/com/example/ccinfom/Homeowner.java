4d289971b59b10227ea75177e3c9c664cdd70575
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-02-07 21:05:06 +0800

    Update Homeowner.java

--- hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
+++ hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
@@ -1,13 +1,15 @@
 package com.example.ccinfom;
 
+import java.util.ArrayList;
 import java.util.List;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 
 //Personal information and contact details
 //Years as homeowner
@@ -22,8 +24,10 @@ public class Homeowner {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @Min(0)
     private int yearsAsHomeowner;
 
     private boolean intentDeclared;
@@ -62,7 +66,7 @@ public class Homeowner {
         public int getYearsAsHomeowner() {
         return yearsAsHomeowner;
     }
-        public boolean getIntentDeclared() {
+        public boolean isIntentDeclared() {
         return intentDeclared;
 
     }
@@ -71,13 +75,33 @@ public class Homeowner {
     }
 
     public void addProperty(Property property) {
-    properties.add(property);
-    property.setHomeowner(this);
+        properties.add(property);
+        property.setHomeowner(this);
     }
     
     public void removeProperty(Property property) {
         properties.remove(property);
         property.setHomeowner(null);
+    }
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setHomeowner(this);
+    }
+    
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+        address.setHomeowner(null);
+    }
+
+    public void addContact(ContactInfo contact) {
+        contacts.add(contact);
+        contact.setHomeowner(this);
+    }
+    
+    public void removeContact(ContactInfo contact) {
+        contacts.remove(contact);
+        contact.setHomeowner(null);
     }
 
 }
