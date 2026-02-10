5605591e9c60c3076826e1de570a145229adf5ec
Liam Sales <liam_miguel_sales@dlsu.edu.ph>
2026-02-10 20:28:03 +0800

    fixed notes for property

--- hoa-system/src/main/java/com/example/ccinfom/Property.java
+++ hoa-system/src/main/java/com/example/ccinfom/Property.java
@@ -5,25 +5,43 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-// Every property in the subdivision has only one homeowner, one household that lives in the property, and maybe several authorized residents in
-// the household. Properties in the subdivision/ is identifiable by a property code. Properties are identified by its block and lot like B06L08. Other
-// details of the property include the (a) size of the property and the (b) date it was turned over to the homeowner.
-// For every property in the subdivision, the household residing in the property must also be registered. This information is being used in order to
-// identify who are residents of the subdivision and for the issuance of the resident’s ID. The authorized Resident/s of the household is responsible
-// for maintaining (adding, modifying or deleting) resident members of the household. A resident member cannot be a member of multiple
-// households. A household is provided with a household ID and is associated with only one property within the subdivision. If the homeowner is
-// an authorized Resident of the household in his property, this means that he is residing in his/her property. A household has several resident
-// HOMEOWNERS ASSOCIATION PROJECT CASE
-// Database Design and DB Applications Development
-// Introduction to Databases (CCINFOM)
-// 2
-// members. When an authorized resident of the household deletes a resident in the household, the resident ID card of the deleted household
-// member is cancelled. It is possible for a resident transfer household. Since resident information is not deleted, at any time, the homeowner is the
+// Every property in the subdivision has only one homeowner, one household that lives in the property,
+//  and maybe several authorized residents in the household.
+// 
+// Properties in the subdivision/ is identifiable by a property code. Properties are identified by its block and lot like B06L08. (natural)
+// Other details of the property include the 
+// (a) size of the property and the 
+// (b) date it was turned over to the homeowner.
+
+// For every property in the subdivision, the household residing in the property must also be registered.
+// This information is being used in order to
+// identify who are residents of the subdivision and for the issuance of the resident’s ID.
+// The authorized Resident/s of the household is responsible
+// for maintaining (adding, modifying or deleting) resident members of the household.
+// A resident member cannot be a member of multiple households.
+// 
+// A household is provided with a household ID and is associated with only one property within the subdivision.
+// If the homeowner is an authorized Resident of the household in his property, this means that he is residing in his/her property.
+// A household has several resident members. 
+// 
+// When an authorized resident of the household deletes a resident in the household, the resident ID card of the deleted household
+// member is cancelled.
+// 
+// It is possible for a resident transfer household. Since resident information is not deleted, at any time, the homeowner is the
 // only one that can change the authorized resident/s to his/her property.
-// For each resident, the following information is needed, (a) Complete name of the resident, (b) renter or not, (c) resident email address, (d)
-// birthday, (e) gender, (f) resident mobile information (g) Facebook URL of the resident if there is any, (h) picture of the resident, (i) relationship to
-// the homeowner, (j) undertaking that the information provided is correct, accurate, and signifies representing being a valid resident. Every resident
-// has a unique Resident ID
+// 
+// For each resident, the following information is needed, -----> can this extend from a superclass?
+// (a) Complete name of the resident,
+// (b) renter or not,
+// (c) resident email address
+// (d) birthday
+// (e) gender
+// (f) resident mobile information
+// (g) Facebook URL of the resident if there is any
+// (h) picture of the resident
+// (i) relationship to the homeowner
+// (j) undertaking that the information provided is correct, accurate, and signifies representing being a valid resident.
+// Every resident has a unique Resident ID
 
 @Entity
 public class Property {
@@ -38,17 +56,3 @@ public class Property {
 
 
 }
-
-//TODO
-//Mark the class as a JPA entity
-//Add a primary key with auto-generation
-//Add fields such as:
-//propertyCode or lotNumber
-//optional metadata like block, phase, size
-//Add a ManyToOne relationship back to Homeowner
-//Prevent infinite JSON recursion by ignoring the homeowner field during serialization
-
-//Provide:
-//protected no-args constructor
-//minimal public constructor
-//getters and setters
