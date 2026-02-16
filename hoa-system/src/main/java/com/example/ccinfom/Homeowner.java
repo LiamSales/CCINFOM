7079b72ec2521b9149b14e8a8cb351e0dfa94f97
Liam Sales <liam_miguel_sales@dlsu.edu.ph>
2026-02-16 14:54:56 +0800

    fixed resident and homeowner

--- hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
+++ hoa-system/src/main/java/com/example/ccinfom/Homeowner.java
@@ -1,32 +1,21 @@
 package com.example.ccinfom;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "homeowner")
-@PrimaryKeyJoinColumn(name = "homeownerid")
+@PrimaryKeyJoinColumn(name = "homeownerid") 
 public class Homeowner extends Individual {
 
     @Column(name = "residency_start")
-    private LocalDate residency_start;
-
+    private LocalDate residencyStart;
 
     @Column(name = "membership", nullable = false)
     private boolean membership;
 
     @Column(name = "isresident", nullable = false)
     private boolean resident;
-
-    @OneToMany(
-            mappedBy = "homeowner",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true
-    )
-    private List<Property> properties = new ArrayList<>();
 
     protected Homeowner() {}
 
@@ -38,51 +27,37 @@ public class Homeowner extends Individual {
             Gender gender,
             String email,
             boolean undertaking,
-            LocalDate residency_start,
+            LocalDate residencyStart,
             boolean membership,
             boolean resident
     ) {
         super(lastname, firstname, mi, birthday, gender, email, undertaking);
-        this.residency_start = residency_start;
+        this.residencyStart = residencyStart;
         this.membership = membership;
         this.resident = resident;
     }
 
-    public LocalDate getResidency_start() {
-        return residency_start;
+    public LocalDate getResidencyStart() {
+        return residencyStart;
+    }
+
+    public void setResidencyStart(LocalDate residencyStart) {
+        this.residencyStart = residencyStart;
     }
 
     public boolean isMembership() {
         return membership;
     }
 
-    public boolean isResident() {
-        return resident;
-    }
-
-    public List<Property> getProperties() {
-        return properties;
-    }
-
-    public void setResidency_start(LocalDate residency_start) {
-        this.residency_start = residency_start;
-    }
-
     public void setMembership(boolean membership) {
         this.membership = membership;
     }
 
+    public boolean isResident() {
+        return resident;
+    }
+
     public void setResident(boolean resident) {
         this.resident = resident;
-    }
-
-    public void addProperty(Property property) {
-        properties.add(property);
-        property.setHomeowner(this);
-    }
-
-    public void removeProperty(Property property) {
-        properties.remove(property);
-        property.setHomeowner(null);
     }
 }
--- hoa-system/src/main/java/com/example/ccinfom/Resident.java
+++ hoa-system/src/main/java/com/example/ccinfom/Resident.java
@@ -8,30 +8,30 @@ import jakarta.persistence.*;
 @PrimaryKeyJoinColumn(name = "residentid")
 public class Resident extends Individual {
 
-    // @ManyToOne(optional = false)
-    // @JoinColumn(name = "property_code")
-    // private Property property;
-
-
     @Column(name = "renter", nullable = false)
     private boolean renter;
-
-    @Column(name = "mobilenum", length = 45)
-    private String mobilenum;
 
     @Column(name = "rel_homeowner", length = 45)
     private String rel_homeowner;
 
-
-    protected Resident() {}
+    protected Resident() {
+        // required by JPA
+    }
 
     public Resident(
+            String lastname,
+            String firstname,
+            String mi,
+            LocalDate birthday,
+            Gender gender,
+            String email,
+            boolean undertaking,
             boolean renter,
             String mobilenum,
             String rel_homeowner
     ) {
+        super(lastname, firstname, mi, birthday, gender, email, undertaking);
         this.renter = renter;
-        this.mobilenum = mobilenum;
         this.rel_homeowner = rel_homeowner;
     }
 
@@ -39,29 +39,15 @@ public class Resident extends Individual {
         return renter;
     }
 
-    public String getMobilenum() {
-        return mobilenum;
+    public void setRenter(boolean renter) {
+        this.renter = renter;
     }
 
     public String getRel_homeowner() {
         return rel_homeowner;
     }
 
-    // public void setProperty(Property property) {
-    //     this.property = property;
-    // }
-
-
-    public void setRenter(boolean renter) {
-        this.renter = renter;
-    }
-
-    public void setMobilenum(String mobilenum) {
-        this.mobilenum = mobilenum;
-    }
-
     public void setRel_homeowner(String rel_homeowner) {
         this.rel_homeowner = rel_homeowner;
     }
-
 }
