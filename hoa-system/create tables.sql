63655edfacc1a7b5a7eab40be3e0e50923facd3a
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-03-04 22:00:40 +0800

    Create create tables.sql

---
+++ hoa-system/create tables.sql
@@ -0,0 +1,28 @@
+DROP DATABASE hoa_db;
+CREATE DATABASE hoa_db;
+USE hoa_db;
+
+CREATE TABLE individual (
+    individualid INT AUTO_INCREMENT PRIMARY KEY,
+    lastname VARCHAR(45) NOT NULL,
+    firstname VARCHAR(45),
+    mi VARCHAR(45),
+    birthday DATE NOT NULL,
+	gender ENUM('M','F') NOT NULL,
+    email VARCHAR(45),
+    facebook_url VARCHAR(45),
+    pic_filename VARCHAR(45),
+    undertaking BOOLEAN NOT NULL
+);
+
+CREATE TABLE mobile (
+    mobilenum VARCHAR(20) NOT NULL,
+    individualid INT NOT NULL,
+    PRIMARY KEY (mobilenum, individualid),
+    CONSTRAINT fk_mobile_individual
+        FOREIGN KEY (individualid)
+        REFERENCES individual(individualid)
+        ON DELETE CASCADE
+);
+
+SHOW TABLES;
